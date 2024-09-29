1bb22abca6e277adceb7f9a114aca2459529bc28
RodrigoWiizz <83847745+RodrigoWiizz@users.noreply.github.com>
2024-09-28 22:57:19 -0300

    feat: funcao de atack implementada

--- Noruega/src/headers/witch/witch.c
+++ Noruega/src/headers/witch/witch.c
@@ -9,9 +9,62 @@
 
 const int DECK_SIZE = 4;
 
-void atack(WITCH* witch)
+enum DAMAGE_STRENGTH getDamageStrenght(enum MIX_RESULTS chemical_reaction,
+        enum WITCH_TYPE witch_type)
 {
+    if ((witch_type == FIRE && chemical_reaction == ACID_BASE) ||
+        (witch_type == WATER && (chemical_reaction == REDOX ||
+            chemical_reaction == TOXIC_COMPOUND_FORMATION)))
+        return SUPER_EFFECTIVE;
+    
+    if ((witch_type == WATER && chemical_reaction == ACID_BASE) ||
+        (witch_type == FIRE && chemical_reaction == REDOX) ||
+        (witch_type == GROUND && chemical_reaction == TOXIC_COMPOUND_FORMATION))
+        return NORMAL;
 
+    if ((witch_type == FIRE && chemical_reaction == TOXIC_COMPOUND_FORMATION) ||
+        (witch_type == GROUND && (chemical_reaction == ACID_BASE ||
+            chemical_reaction == REDOX)))
+        return NOT_EFFECTIVE;
+}
+
+void atack(WITCH* attacker, WITCH* target, 
+        enum CHEMICAL_ELEMENTS chosen_element,
+        enum CHEMICAL_ELEMENTS central_element)
+{
+    enum MIX_RESULTS chemical_reaction = mixElements(chosen_element, central_element);
+    
+    if (chemical_reaction == COMPATIBLE)
+    {
+        printf_s("\nO Atacante ganhou mais 10 de vida");
+        attacker->health_points += 10;
+    }
+    else if (chemical_reaction == METAL_REACTIVITY)
+    {
+        if (target->damage_received_multiplier < 1.8)
+        {
+            printf_s("\nDefesa do oponente diminuida");
+            target->damage_received_multiplier += 0.2;
+        }
+        else
+            printf_s("\nNão é possível abaixar mais a defesa do oponente");
+    }
+    else if (chemical_reaction == WATER_REACTIVITY)
+    {
+        if (attacker->damage_received_multiplier > 0.2)
+        {
+            printf_s("\nDefesa do atacante aumentada");
+            attacker->damage_received_multiplier -= 0.2;
+        }
+        else
+            printf_s("\nNão é possível aumentar mais a defesa do atacante");
+    }
+    else
+    {
+        int damage = getDamageStrenght(chemical_reaction, target->type) * target->damage_received_multiplier;
+        target->health_points -= damage;
+        printf_s("\nDano causado pelo atacante no oponente: %i", damage);
+    }
 }
 
 ALLEGRO_BITMAP* initWitchSprite(char image_path[])
@@ -52,9 +105,10 @@ WITCH * initWitch(char image_path[], int coordinate_x, int coordinate_y, int hea
     witch->destroyWitch = destroyWitch;
     witch->coordinate_x = coordinate_x;
     witch->coordinate_y = coordinate_y;
+    witch->damage_received_multiplier = 1.0;
     witch->health_points = health_points;
     witch->type = type;
     witch->deck = (enum CHEMICAL_ELEMENTS *) generateRandomIntArrayInRange(DECK_SIZE, ELEMENTS_AMOUNT);
-
+    witch->atack = atack;
     return witch;
 }
--- Noruega/src/headers/witch/witch.h
+++ Noruega/src/headers/witch/witch.h
@@ -3,6 +3,13 @@
 	
 	extern const int DECK_SIZE;
 
+	enum DAMAGE_STRENGTH
+	{
+		NOT_EFFECTIVE = 15,
+		NORMAL = 30,
+		SUPER_EFFECTIVE = 45
+	};
+
 	enum WITCH_TYPE
 	{
 		WATER,
@@ -18,12 +25,14 @@
 		ALLEGRO_BITMAP* sprite;
 		enum CHEMICAL_ELEMENTS * deck;
 		int health_points;
+		float damage_received_multiplier;
 		int coordinate_x;
 		int coordinate_y;
 		//-- Funções --// Criar função para mudar a imagem?
 		void (*drawWitch)(WITCH*);
 		void (*destroyWitch)(WITCH*);
 		// atack()
+		void (*atack)(WITCH*, WITCH*, enum CHEMICAL_ELEMENTS, enum CHEMICAL_ELEMENTS);
 	};
 
 	WITCH* initWitch(char image_path[], int coordinate_x, int coordinate_y, int health_points, enum WITCH_TYPE type);
